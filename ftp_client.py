e2d251eb87180b9a7b11420e8b0b0cfaa77d2a12
nick_mattis <nickm970@gmail.com>
2018-04-30 16:32:33 -0400

    init commit of project

---
+++ ftp_client.py
@@ -0,0 +1,14 @@
+"""
+ftp_client.py
+Nick Mattis
+
+Connects to a specified ftp_server, allows for the downloading
+or uploading of files.
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ ftp_server.py
@@ -0,0 +1,15 @@
+"""
+ftp_server.py
+Nick Mattis
+
+Runs on the host, starts listening for incoming requests from clients.
+Checks user name, password and allows for the getting and putting of
+files from client to server.
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
